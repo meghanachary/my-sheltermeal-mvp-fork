8ac9c14cf6a900234b9c18b45d58f0d707116c6c
MEG <MEGHCHARY@GMAIL.COM>
2024-03-31 20:55:06 -0700

    Installed leaflet and react leaflet. My MapFeature component now has a working map with NYC coordinates. Imported a couple more packages from react leaflet which will be used later for marker and popup so users can select locations from the map.

--- client/src/components/JoinUs.jsx
+++ client/src/components/JoinUs.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 
-
 const JoinUs = () => {
-    return (
-        <div className="join-section">
-        <h2 className="join-title">Join Us</h2>
-        <p className="join-text">Did you know that every donation helps to make a difference in our community? </p>
-        <p className="join-text"> Join us to make the world a better place by helping those in need. Together, we can provide meals to shelters and support those facing food insecurity.</p>
-        <p className="join-text">Please feel free to contact us at mealshelter@gmail.com</p>
-        <p className="join-text">If you can't feed a hundred people, then just feed one, "Mother Theresa".</p>
-        </div>
-    );
-}
+  return (
+    <div className="join-section">
+      <h2 className="join-title">Join Us</h2>
+      <p className="join-text">
+        Did you know that every donation helps to make a difference in our
+        community?{" "}
+      </p>
+      <p className="join-text">
+        {" "}
+        Join us to make the world a better place by helping those in need.
+        Together, we can provide meals to shelters and support those facing food
+        insecurity.
+      </p>
+      <p className="join-text">
+        Please feel free to contact us at mealshelter@gmail.com
+      </p>
+      <p className="join-text">
+        If you can't feed a hundred people, then just feed one, "Mother
+        Theresa".
+      </p>
+    </div>
+  );
+};
 
-
-
-
-export default JoinUs;        
+export default JoinUs;
--- client/src/components/MapFeature.jsx
+++ client/src/components/MapFeature.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 // import { Link } from "react-router-dom"; // keeping it here in case this is needed
 
+// Importing various things we will need for our map
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
+
 const MapFeature = () => {
-  return <h1>This will be the Map Feature</h1>;
+  return (
+    <MapContainer center={[40.7128, -74.006]} zoom={16} scrollWheelZoom={false}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  );
 };
 export default MapFeature;
