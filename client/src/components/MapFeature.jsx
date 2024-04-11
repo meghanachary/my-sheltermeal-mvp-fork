e48197d8f1dcb9e9a08e5223299539f012500b1e
MEG <MEGHCHARY@GMAIL.COM>
2024-04-10 21:15:06 -0700

    Deleted the previous code I used for the markers to test the functionality

--- client/src/components/MapFeature.jsx
+++ client/src/components/MapFeature.jsx
@@ -8,46 +8,6 @@ import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon } from "leaflet";
 
-// // Below I created an array of locations to test the marker/popup functionality but it would be better to fetch directly from the API.
-// const markers = [
-//   {
-//     geocode: [40.774929, -73.957207],
-//     popup: "Masseria, 1404 3rd Ave New York NY",
-//   },
-//   {
-//     geocode: [42.72911, -73.70171],
-//     popup: "The Red Grill, 1701 2nd Ave New York NY",
-//   },
-//   {
-//     geocode: [42.72541, -73.70244],
-//     popup: "Mission Ceviche, 1400 2nd Ave New York NY",
-//   },
-//   {
-//     geocode: [40.7613, -73.96111],
-//     popup: "Sea Salt NYC, 1123 1st Ave New York NY",
-//   },
-//   {
-//     geocode: [40.77545, -73.96306],
-//     popup: "Sant Ambroeus, 1000 Madison Ave New York NY",
-//   },
-//   {
-//     geocode: [40.78561, -73.97259],
-//     popup: "The Viand, 517 Columbus Ave New York NY",
-//   },
-//   {
-//     geocode: [40.779339, -73.977623],
-//     popup: "Pappardella, 316 Columbus Ave New York NY",
-//   },
-//   {
-//     geocode: [40.787018, -73.977971],
-//     popup: "Maison Pickle, 2315 Broadway Ave New York NY ",
-//   },
-//   {
-//     geocode: [40.77697, -73.979332],
-//     popup: "Felice 71, 240 Columbus Ave New York NY",
-//   },
-// ];
-
 const MapFeature = () => {
   const [restaurantData, setRestaurantData] = useState([]);
   const [shelterData, setShelterData] = useState([]);
@@ -89,14 +49,7 @@ const MapFeature = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        {/*Below code is the markers + popups for the 'markers' array of objects */}
-        {/* {markers.map((marker) => (
-          <Marker position={marker.geocode}>
-            <Popup>
-              <h4>{marker.popup}</h4>
-            </Popup>
-          </Marker>
-        ))} */}
+
         {/*Mapping through the restaurants to create markers */}
         {restaurantData.map((restaurant, index) => (
           <Marker
@@ -111,7 +64,7 @@ const MapFeature = () => {
             </Popup>
           </Marker>
         ))}
-        Mapping through the shelters to create markers
+        {/* Mapping through the shelters to create markers */}
         {shelterData.map((shelter, index) => (
           <Marker key={index} position={[shelter.latitude, shelter.longitude]}>
             <Popup>
